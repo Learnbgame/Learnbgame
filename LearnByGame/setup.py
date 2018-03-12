25f1045bd3f6071bb8b57d0520d115150d8b89ca
ggzhang0071 <34293109+ggzhang0071@users.noreply.github.com>
2018-03-12 14:13:22 +0800

    Add files via upload

---
+++ LearnByGame/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	author = 'Fofight'
+	name = 'LearnByGame',
+	version = '1.0',
+	packages = find_packages(),
+	description = "Learn by game",
+	license = "GPL",
+	keywords = "Learnbgame",
+	url = "learnbgame.com",	
+
+
+	)
